771b780e082c485be0dda5256c9fc0cebdd1caea
user <nikacuckiridze47@gmail.com>
2023-04-24 20:20:40 +0400

    test: add command test

--- tests/Feature/Command/FetchDataTest.php
+++ tests/Feature/Command/FetchDataTest.php
@@ -2,17 +2,24 @@
 
 namespace Tests\Feature\Command;
 
+use App\Console\Commands\FetchData;
+use App\Models\Statistic;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class FetchDataTest extends TestCase
 {
-	// /**
-	//  * A basic feature test example.
-	//  */
-	// public function test_example(): void
-	// {
-	//     $response = $this->get('/');
+	use RefreshDatabase;
 
-	//     $response->assertStatus(200);
-	// }
+	public function test_statistics_database_is_cleared_at_start()
+	{
+		$request = Http::get('https://devtest.ge/countries');
+		$countries = json_decode($request->getBody()->getContents(), true);
+		$size = sizeof($countries);
+		$this->artisan(FetchData::class)->execute();
+		$last = Statistic::all()->last();
+		$this->assertEquals($last['code'], 'worldwide');
+		$this->assertEquals(Statistic::all()->count(), $size + 1);
+	}
 }
