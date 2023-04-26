9e8fc2bff49e86c249e3972789ef4bbf15944c3a
user <nikacuckiridze47@gmail.com>
2023-04-26 15:23:59 +0400

    fix: make only logo clickable

--- resources/views/components/auth-page.blade.php
+++ resources/views/components/auth-page.blade.php
@@ -1,8 +1,11 @@
 <div class="flex  ">
     <div class="lg:flex-1 px-4 pt-6 md:pl-28 md:pt-10 mx-auto">
-        <a href="{{ route('home') }}">
-            <img src="{{ asset("assets/logo.svg") }}" class=" mb-10 md:mb-12 w-40 h-auto"/>
-        </a>
+        <div class="flex mb-10 md:mb-12">
+            <a href="{{ route('home') }}">
+                <img src="{{ asset("assets/logo.svg") }}" class="  w-40 h-auto"/>
+            </a>
+    
+        </div>
         {{ $slot }}
     </div>
     <img src="{{ asset("assets/side-pic.png") }}" alt="side pic"
