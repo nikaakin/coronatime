e12318d8efb5ec506fadc3f1f1d7c8db1c10495b
user <nikacuckiridze47@gmail.com>
2023-04-26 15:42:48 +0400

    fix: add burger menu width

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex flex-1 px-4 pt-6 md:px-28 md:pt-10 justify-between text-slate-970 pb-6 md:pb-5 mb-6 md:mb-10 items-center border-b-[1px] border-neutral-150">
+<nav class="flex gap-1 flex-1 px-4 pt-6 md:px-28 md:pt-10 justify-between text-slate-970 pb-6 md:pb-5 mb-6 md:mb-10 items-center border-b-[1px] border-neutral-150">
     <a href="{{ route('home') }}"  >
         <img src="{{ asset("assets/logo.svg") }}" class="  w-40 h-auto"/>
     </a>
